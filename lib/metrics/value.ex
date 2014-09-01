4276b6516f42562eebce7b303408acbfb3da44be
JC Grubbs <jc.grubbs@devmynd.com>
2014-08-31 19:38:59 -0500

    basic tests for value metric

--- lib/metrics/value.ex
+++ lib/metrics/value.ex
@@ -1,16 +1,71 @@
 defmodule Tabs.Metrics.Value do
+  use Timex
   use Exredis
+  use Jazz
   alias Tabs.Resolution
 
-  def record(bucket, name, value, timestamp, resolutions) do
-    client = Exredis.start
+  def record(client, bucket, name, value, timestamp = %DateTime{}, resolutions) when is_number(value) do
     Enum.each(resolutions, fn(unit) ->
+      key = storage_key(bucket, name, unit, timestamp)
+      { :ok, current } = case client |> query(["GET", key]) do
+        :undefined -> { :ok, default_data }
+        value -> JSON.decode(value, keys: :atoms)
+      end
+      { :ok, updated } = new_data(current, value) |> JSON.encode
+      client |> query(["SET", key, updated])
     end)
   end
 
   def storage_key(bucket, name, resolution, timestamp) do
     formatted_time = Resolution.serialize(resolution, timestamp)
     "stat:#{bucket}:value:#{name}:#{resolution}:#{formatted_time}"
+  end
+
+  defp default_data do
+    %{ count: 0, sum: 0.00, min: nil, max: nil, avg: 0.00, trail: [], trend: "hold" }
+  end
+
+  defp new_data(current, value) do
+    %{ current |
+      count: updated_count(current),
+      sum: updated_sum(current, value),
+      min: updated_min(current, value),
+      max: updated_max(current, value),
+      avg: updated_avg(current, value),
+      trail: updated_trail(current, value),
+      trend: updated_trend(current, value)
+    }
+  end
+
+  defp updated_count(%{ count: count }), do: count + 1
+  defp updated_avg(%{ sum: sum, count: count }, value), do: (sum + value) / (count + 1)
+  defp updated_sum(%{ sum: sum }, value), do: sum + value
+  defp updated_trail(%{ trail: trail }, value), do: [value] ++ List.delete_at(trail, 19)
+
+  defp updated_min(%{ min: min }, value) do
+    cond do
+      min == nil -> value
+      min >= value -> value
+      min < value -> min
+    end
+  end
+
+  defp updated_max(%{ max: max }, value) do
+    cond do
+      max == nil -> value
+      max <= value -> value
+      max > value -> max
+    end
+  end
+
+  defp updated_trend(%{ trail: [] }, value), do: "hold"
+
+  defp updated_trend(%{ trail: [prev|_] }, value) do
+    cond do
+      prev == value -> "hold"
+      prev < value -> "up"
+      prev > value -> "down"
+    end
   end
 
 end
--- test/metrics/value_test.exs
+++ test/metrics/value_test.exs
@@ -1,6 +1,8 @@
 defmodule ValueTest do
   use ExUnit.Case
   use Timex
+  use Exredis
+  use Jazz
   alias Tabs.Metrics.Value
 
   setup do
@@ -13,6 +15,49 @@ defmodule ValueTest do
   test "storage_key() generates the correct keys", %{ timestamp: timestamp } do
     assert Value.storage_key(:foo, :bar, :minute, timestamp) == "stat:foo:value:bar:minute:2014-03-17-03-55"
     assert Value.storage_key(:baz, :boo, :day, timestamp) == "stat:baz:value:boo:day:2014-03-17"
+  end
+
+  test "record() sets the correct keys in redis", %{ timestamp: timestamp, client: client } do
+    Value.record(client, :foo, :bar, 42, timestamp, Tabs.default_resolutions)
+    Enum.each(Tabs.default_resolutions, fn(unit) ->
+      assert Exredis.query(client, ["GET", Value.storage_key(:all, :foo, unit, timestamp)]) != nil
+    end)
+  end
+
+  test "record() correctly updates sum, count, and average", %{ timestamp: timestamp, client: client } do
+    key = Value.storage_key(:foo, :bar, :hour, timestamp)
+    Value.record(client, :foo, :bar, 42, timestamp, Tabs.default_resolutions)
+    Value.record(client, :foo, :bar, 99.3, timestamp, Tabs.default_resolutions)
+    Value.record(client, :foo, :bar, 18.54, timestamp, Tabs.default_resolutions)
+    { :ok, data } = client |> query(["GET", key]) |> JSON.decode(keys: :atoms)
+    assert data.count == 3
+    assert data.sum == 159.84
+    assert data.avg == 53.28
+  end
+
+  test "record() correctly sets the min, max, trail, and trend", %{ timestamp: timestamp, client: client } do
+    key = Value.storage_key(:foo, :bar, :hour, timestamp)
+
+    Value.record(client, :foo, :bar, 42, timestamp, Tabs.default_resolutions)
+    { :ok, data } = client |> query(["GET", key]) |> JSON.decode(keys: :atoms)
+    assert data.min == 42
+    assert data.max == 42
+    assert data.trail == [42]
+    assert data.trend == "hold"
+
+    Value.record(client, :foo, :bar, 99.3, timestamp, Tabs.default_resolutions)
+    { :ok, data } = client |> query(["GET", key]) |> JSON.decode(keys: :atoms)
+    assert data.min == 42
+    assert data.max == 99.3
+    assert data.trail == [99.3, 42]
+    assert data.trend == "up"
+
+    Value.record(client, :foo, :bar, 18.54, timestamp, Tabs.default_resolutions)
+    { :ok, data } = client |> query(["GET", key]) |> JSON.decode(keys: :atoms)
+    assert data.min == 18.54
+    assert data.max == 99.3
+    assert data.trail == [18.54, 99.3, 42]
+    assert data.trend == "down"
   end
 
 end
